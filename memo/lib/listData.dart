ddbc1b6161b7a91f3dccd5d22b40218ad835236e
chemoontheshy <xzfandzgx@gmail.com>
2021-01-10 00:37:39 +0800

    add some flutter use method

---
+++ memo/lib/listData.dart
@@ -0,0 +1,32 @@
+List listData = [
+  {
+    "title": 'Candy Shop',
+    "author": 'Mohamed Chahin',
+    "imageUrl": 'https://www.itying.com/images/flutter/1.png',
+  },
+  {
+    "title": 'Childhood in a picture',
+    "author": 'Goolge',
+    "imageUrl": 'https://www.itying.com/images/flutter/2.png',
+  },
+  {
+    "title": 'Alibaba Shop',
+    "author": 'Alibaba',
+    "imageUrl": 'https://www.itying.com/images/flutter/3.png',
+  },
+  {
+    "title": 'Candy Shop',
+    "author": 'Mohamed Chahin',
+    "imageUrl": 'https://www.itying.com/images/flutter/4.png',
+  },
+  {
+    "title": 'Tornado',
+    "author": 'Mohamed Chahin',
+    "imageUrl": 'https://www.itying.com/images/flutter/5.png',
+  },
+  {
+    "title": 'Candy Shop',
+    "author": 'Mohamed Chahin',
+    "imageUrl": 'https://www.itying.com/images/flutter/6.png',
+  },
+];
--- memo/lib/main.dart
+++ memo/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'listData.dart';
 
 void main() {
   runApp(new MyApp());
@@ -12,13 +15,14 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Memo'),
         ),
-        body: ImageContent(),
+        body: ListViewBuilderContent(),
       ),
       theme: ThemeData(primarySwatch: Colors.yellow),
     );
   }
 }
 
+/// 1.1 基本：组成分析
 class HomeCenter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -36,6 +40,7 @@ class HomeCenter extends StatelessWidget {
   }
 }
 
+/// 1.1 基本：Container,Text样式
 class HomeContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Center(
@@ -64,22 +69,158 @@ class HomeContent extends StatelessWidget {
       ));
 }
 
+/// 2.1 图片：加载图片network,asset
 class ImageContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Center(
         child: Container(
-          child: Image.network(
-            "https://s2.ax1x.com/2019/06/23/Z9jRNd.jpg",
-            alignment: Alignment.topLeft,
-            //color: Colors.blue,
-            //colorBlendMode: BlendMode.screen,
-            fit: BoxFit.cover,
-          ),
+          //1.显示图片
+          // child: Image.network(
+          //   "https://s2.ax1x.com/2019/06/23/Z9jRNd.jpg",
+          //   //alignment: Alignment.topLeft,
+          //   //color: Colors.blue,
+          //   //对比色
+          //   //colorBlendMode: BlendMode.screen,
+          //   //覆盖拉伸
+          //   //fit: BoxFit.cover,
+          //   //repeat: ImageRepeat.repeatY,
+          // ),
           width: 300.0,
           height: 300.0,
           decoration: BoxDecoration(
-            color: Colors.yellow,
+              color: Colors.yellow,
+              borderRadius: BorderRadius.circular(150),
+              image: DecorationImage(
+                  image: NetworkImage(
+                      "https://s2.ax1x.com/2019/06/23/Z9jRNd.jpg"))),
+        ),
+      );
+}
+
+/// 2.2 图片：flutter的CilpOval用法
+class CilpOvalContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Center(
+        child: Container(
+          child: ClipOval(
+            child: Image.network(
+              "https://s2.ax1x.com/2019/06/23/Z9jRNd.jpg",
+              width: 100,
+              height: 100,
+              fit: BoxFit.cover,
+            ),
           ),
         ),
       );
 }
+
+/// 3.1 列表：scrollDirection
+class ListViewContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Container(
+      height: 180,
+      child: ListView(
+        padding: EdgeInsets.all(10),
+        scrollDirection: Axis.horizontal,
+        // children: [
+        //   ListTile(
+        //     leading: Icon(
+        //       Icons.home,
+        //       color: Colors.red,
+        //     ),
+        //     title: Text("一级标题"),
+        //     subtitle: Text("二级标题"),
+        //   )
+        // ],
+        children: [
+          Container(
+            width: 180,
+            height: 180,
+            color: Colors.red,
+          ),
+          Container(
+            width: 180,
+            height: 180,
+            color: Colors.green,
+          ),
+          Container(
+            width: 180,
+            height: 180,
+            color: Colors.blue,
+          ),
+          Container(
+            width: 180,
+            height: 180,
+            color: Colors.deepOrange,
+          ),
+          Container(
+            width: 180,
+            height: 180,
+            color: Colors.pink,
+          ),
+          Container(
+            width: 180,
+            height: 180,
+            color: Colors.purple,
+          ),
+        ],
+      ));
+}
+
+/// 3.2 列表：动态列表
+class DynamicListContent extends StatelessWidget {
+  //自定义方法
+  List<Widget> _getData() {
+    List<Widget> mylist = new List();
+    for (var i = 0; i < 20; i++) {
+      mylist.add(ListTile(
+        title: Text(" i am $i list"),
+      ));
+    }
+    return mylist;
+  }
+
+  @override
+  Widget build(BuildContext context) => Container(
+        child: ListView(
+          children: this._getData(),
+        ),
+      );
+}
+
+/// 3.3 列表：动态循环，传统方式
+class GetListContent extends StatelessWidget {
+  List<Widget> _getData() {
+    var tempList = listData.map((value) => ListTile(
+          leading: Image.network(value["imageUrl"]),
+          title: Text(value["title"]),
+          subtitle: Text(value["author"]),
+        ));
+    return tempList.toList();
+  }
+
+  @override
+  Widget build(BuildContext context) => Container(
+        child: ListView(
+          children: _getData(),
+        ),
+      );
+}
+
+/// 3.4列表：动态循环，flutter的listView.builder用法
+class ListViewBuilderContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Container(
+        child: ListView.builder(
+          itemCount: listData.length,
+          itemBuilder: (context, index) => ListTile(
+            leading: Image.network(listData[index]["imageUrl"]),
+            title: Text(listData[index]["title"]),
+            subtitle: Text(listData[index]["author"]),
+          ),
+        ),
+      );
+}
+
+/// 4.1 网格布局：GridView.count
+/// 4.2 网格布局：GridView.Builder
